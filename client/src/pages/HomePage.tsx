dbccc9d8cee4ee260fe24a99d53897ccff6c507e
JLRWOLF44 <jeanlouisroger44@gmail.com>
2024-12-06 10:21:53 +0100

    creat footer et mofication HomePage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,57 +1,28 @@
 import { NavLink } from "react-router-dom";
 import "./homePage.css";
+
 function HomePage() {
   const articles = [
-    {
-      image:
-        "https://conservewildcats.org/wp-content/uploads/sites/5/2017/10/Westerlund_Katerina_5.jpg",
-      altText: "Article 1",
-      link: "#",
-    },
-    {
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Ara_macao_qtl1.jpg/1200px-Ara_macao_qtl1.jpg",
-      altText: "Article 2",
-      link: "#",
-    },
-    {
-      image:
-        "https://th.bing.com/th/id/OIP.2BM16BCsijy4fA9SVxDq1QAAAA?rs=1&pid=ImgDetMain",
-      altText: "Article 3",
-      link: "#",
-    },
-    {
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/99/African_Elephant.jpg",
-      altText: "Article 4",
-      link: "#",
-    },
-    {
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/99/African_Elephant.jpg",
-      altText: "Article 5",
-      link: "#",
-    },
-
-    {
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/99/African_Elephant.jpg",
-      altText: "Article 6",
-      link: "#",
-    },
+    { image: "https://picsum.photos/200/300", altText: "Article 1", link: "#" },
+    { image: "https://picsum.photos/200/301", altText: "Article 2", link: "#" },
+    { image: "https://picsum.photos/200/302", altText: "Article 3", link: "#" },
+    { image: "https://picsum.photos/200/303", altText: "Article 4", link: "#" },
+    { image: "https://picsum.photos/200/304", altText: "Article 5", link: "#" },
+    { image: "https://picsum.photos/200/305", altText: "Article 6", link: "#" },
   ];
 
   return (
     <main>
       <section id="recent-articles">
         {articles.map((article) => (
-          <NavLink
-            to={article.link}
-            key={article.altText}
-            className="clickable-card"
-          >
-            <img src={article.image} alt={article.altText} />
-          </NavLink>
+          <div className="clickable-card" key={article.altText}>
+            <NavLink to={article.link}>
+              <img src={article.image} alt={article.altText} />
+            </NavLink>
+            <a href="/" className="like-dons">
+              Dons
+            </a>
+          </div>
         ))}
       </section>
     </main>
